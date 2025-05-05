e4e795385014b5dde2e5094db105bc14932abf02
Αƙᴜα • 海 <sn.akua06@gmail.com>
2025-05-05 13:20:16 +0530

    Update bot_uptime.py

--- bot_uptime.py
+++ bot_uptime.py
@@ -1,3 +1,6 @@
+
+
+#Define this function to get readable uptime. 
 def get_readable_time(seconds: int) -> str:
     days, remainder = divmod(seconds, 86400) 
     hours, remainder = divmod(remainder, 3600) 
@@ -16,5 +19,42 @@ def get_readable_time(seconds: int) -> str:
     return " ".join(time_parts)
 
 
+# Add this part where you want to define the command
 
-uptime = get_readable_time(int(time.total_seconds()))
+from pytz import timezone
+import get_readable_time
+from urllib.parse import quote
+
+tz = timezone("Asia/Kolkata") #specify your timezone 
+st = datetime.now(tz) 
+
+
+@Client.on_message(filters.command("uptime"))
+async def usage_cmd(client: Client, message: Message):
+    #Bot Uptime 
+    now = datetime.now(tz)
+    time = now - st
+    uptime = get_readable_time(int(time.total_seconds()))
+    start_time = st.strftime('%Y-%m-%d %H:%M:%S')  
+    
+    # Final message construction
+    msg = (
+        f"<blockquote>**⏱️Uptime :**</blockquote>\n\n"
+        f"**➩BOT UPTIME:** `{uptime}`🔥\n"
+        f"**💙𝐋αѕ𝖙 Ꮢєst𝐚ʀᴛ:** `{start_time} (IST)`"
+    ) 
+    
+    
+    await message.reply_text(msg, quote=True)
+    
+
+#end 
+
+
+
+
+# Don't Remove Credits 🥹
+# By:- @Akua-06
+# Report errors at :- https://t.me/sn_akua_bot 
+# Happy Coding 🥰
+
