e054921f43b566d118919d447d450baae7447a19
Αƙᴜα • 海 <sn.akua06@gmail.com>
2025-05-05 16:28:28 +0530

    Update config.py

--- bot_commands/config.py
+++ bot_commands/config.py
@@ -1,1 +1,6 @@
-#will be updated soon..... 
+#Add this to the config.py file of your bot repo. 
+
+import os
+
+AUTO_SET_COMMANDS = str(os.environ.get("AUTO_SET_COMMANDS", "True"))
+# Enable auto setting bot commands on startup, (default: True)
